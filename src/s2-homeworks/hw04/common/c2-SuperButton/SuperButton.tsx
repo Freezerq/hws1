c3bcfc34ac55a7533a11e66322c3775a4e45211d
Freezerq <thepazdk@gmail.com>
2023-01-04 13:55:32 +0600

    hw4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,15 +17,22 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
+
     const finalClassName = s.button
-        + (disabled ? ' ' +  s.disabled: '')
-        + (xType === 'red'? ' ' +  s.red: '')
-        + (xType === 'secondary'? ' ' + s.secondary: '')
-        + (className ? ' ' + className : '') // задачка на смешивание классов
-    console.log(finalClassName)
+        + (disabled ? ' ' + s.disabled : '')
+        + (xType === 'red' ? ' ' + s.red : '')
+        + (xType === 'secondary' ? ' ' + s.secondary : ' ' + s.default)
+    //     + (className ? ' ' + className : '') // задачка на смешивание классов
+    // const finalClassName = s.button + (
+    //     disabled ? ' ' + s.disabled
+    //         : xType === 'red'
+    //             ? ' ' + s.red
+    //             : xType === 'secondary'
+    //                 ? ' ' + s.secondary
+    //                 : ' ' + s.default)
+    // + (className ? ' ' + className : '')
 
     return (
-
         <button
             disabled={disabled}
             className={finalClassName}
