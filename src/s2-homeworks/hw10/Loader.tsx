afc6b8f2a96a0300582a52987d2651fd2a072daf
Freezerq <thepazdk@gmail.com>
2023-01-29 21:34:59 +0600

    hw10 Did all

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,4 @@
 import s from './Loader.module.css'
+import loader from '../../hearts.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div style={{ width: '150px', height: '70px'}} ><img className={s.loader} src={loader} alt=""/></div>
